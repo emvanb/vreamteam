4276eca083b5fdde3e0f37d3d37415b9965a57b6
tinashe <tinashechagwedera@gmail.com>
2016-10-08 09:49:30 -0400

    point class has creation time

--- PitchPaint/Assets/Scripts/Point.cs
+++ PitchPaint/Assets/Scripts/Point.cs
@@ -5,15 +5,24 @@ using System;
 
 public class LinePoint
 {
+
+
 	// contains vector3 to for ponit location adn vector3 for velocity
 	public Vector3 PointLocation{
 		get;
 		set;
 	}
 
-	public Vector3 Velocity{
+	public Vector3 Velocity{ // velocity of point
 		get;
 		set;
 
 	}
+
+	public float CreationTime {
+		get;
+		set;
+
+	}
+		
 }
