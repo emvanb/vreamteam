502dfac7acfbe27c77633891f67d2cca4f2aac8c
Emily Van Belleghem <evanb@mit.edu>
2016-10-08 10:42:25 -0400

    added base brush class

--- PitchPaint/Assets/Scripts/BaseBrush.cs
+++ PitchPaint/Assets/Scripts/BaseBrush.cs
@@ -2,7 +2,10 @@
 using System.Collections;
 
 public class BaseBrush : MonoBehaviour {
-	AudioSource sample;
+	public AudioSource sample;
+	float timeBetweenPoints =1;
+	float currentTime = 0;
+	Vector3 lastPos = Vector3.zero;
 	// Use this for initialization
 	void Start () {
 	
@@ -10,16 +13,24 @@ public class BaseBrush : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
 	}
 
 	void PlaySample()
 	{
-
+		sample.Play ();
 	}
 
 	void UpdateDraw(Vector3 handPos, Line currentLine)
 	{
-
+		currentTime += Time.deltaTime;
+		if (currentTime > timeBetweenPoints) {
+			LinePoint pt = new LinePoint ();
+			pt.CreationTime = Time.time - currentLine.startTime;
+			pt.PointLocation = handPos;
+			pt.Velocity = (handPos - lastPos).normalized;
+			currentLine.AddPoint(pt);
+		}
+		lastPos = handPos;
 	}
 }
