db6df17c20964b52a0a4264577bd7428b19d7da6
Zach Sherin <zarrro45@gmail.com>
2016-10-08 13:25:20 -0400

    adding EVBs input code

---
+++ PitchPaint/Assets/Scripts/GameLoop.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameLoop : MonoBehaviour {
+    private TextMesh effectText;
+    private TextMesh soundText;
+    public TextMesh console;
+    private Vream_Controller leftController;
+    private Vream_Controller rightController;
+    private int soundName = 0;
+    private int effectName = 0;
+    // Use this for initialization
+    void Start () {
+        effectText = GameObject.Find("Effect_Text").GetComponent<TextMesh>();
+        soundText = GameObject.Find("Sound_Text").GetComponent<TextMesh>();
+        rightController = GameObject.Find("Controller (right)").GetComponent<Vream_Controller>();
+        leftController = GameObject.Find("Controller (left)").GetComponent<Vream_Controller>();
+        console = GameObject.Find("Console").GetComponent<TextMesh>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        console.text ="heyyy!!";
+        if (rightController.triggerPress)
+        {
+            console.text = "yo yo trigger pressed on right";
+        }
+
+        if (rightController.dpadPressRight)
+        {
+            console.text = "right controller right";
+
+            soundText.text = "Sound_" + (soundName + 1).ToString();
+        }
+
+        if (rightController.dpadPressLeft)
+        {
+            console.text = "right controllerleft";
+
+            soundText.text = "Sound_" + (soundName - 1).ToString();
+        }
+        if (leftController.dpadPressRight)
+        {
+            console.text = "leftcontroller right";
+
+            soundText.text = "Effect_" + (soundName + 1).ToString();
+        }
+
+        if (leftController.dpadPressLeft)
+        {
+            console.text = "leftcontrollerleft";
+
+            soundText.text = "Effect_" + (soundName - 1).ToString();
+        }
+    }
+}
---
+++ PitchPaint/Assets/Scripts/Vream_Controller.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Vream_Controller : MonoBehaviour {
+
+    public bool triggerPress = false;
+    public bool dpadPressUp = false;
+    public bool dpadPressDown = false;
+    public bool dpadPressLeft = false;
+    public bool dpadPressRight = false;
+    //coordinates are a float from 0-1. Where origin is center  of the touch pad and  top right corner  is 1,1
+    private float dpadCoordX = 0;
+    private float dpadCoordY=0;
+
+	private Valve.VR.EVRButtonId triggerButton = Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger;
+    private Valve.VR.EVRButtonId padButton = Valve.VR.EVRButtonId.k_EButton_Axis1;
+
+    private SteamVR_Controller.Device controller {
+		get { return SteamVR_Controller.Input((int)trackedObj.index);
+		}
+    }
+
+	public SteamVR_TrackedObject trackedObj;
+	void Start() {
+		//trackedObj = GetComponent();
+	}
+
+	void Update() {
+        triggerPress = false;
+        dpadPressUp = false;
+        dpadPressDown = false;
+        dpadPressLeft = false;
+        dpadPressRight = false;
+        dpadCoordX = controller.GetAxis(Valve.VR.EVRButtonId.k_EButton_Axis0).x;
+        dpadCoordY = controller.GetAxis(Valve.VR.EVRButtonId.k_EButton_Axis0).y;
+
+        if (controller == null) {
+			Debug.Log("Controller not initialized");
+			return;
+		}
+
+        triggerPress = controller.GetPressDown(triggerButton);
+		if (triggerPress) {
+			Debug.Log("trigger  down");
+		}
+
+        if (controller.GetPressDown(Valve.VR.EVRButtonId.k_EButton_DPad_Up))
+        {
+            dpadPressUp = true;
+        }
+
+        if (controller.GetPressDown(Valve.VR.EVRButtonId.k_EButton_DPad_Down))
+        {
+            dpadPressDown = true;
+        }
+
+        if (controller.GetPressDown(Valve.VR.EVRButtonId.k_EButton_DPad_Left))
+        {
+            dpadPressLeft = true;
+        }
+
+        if (controller.GetPressDown(Valve.VR.EVRButtonId.k_EButton_DPad_Right))
+        {
+            dpadPressRight = true;
+        }
+    }
+
+}
+
+
