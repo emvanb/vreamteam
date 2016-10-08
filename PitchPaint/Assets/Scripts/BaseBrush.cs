222584f4f76dfa9b10a6b1f63c7ddcf99b6c5650
Emily Van Belleghem <evanb@mit.edu>
2016-10-08 10:11:08 -0400

    added base brush class

---
+++ PitchPaint/Assets/Scripts/BaseBrush.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaseBrush : MonoBehaviour {
+	AudioSource sample;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void PlaySample()
+	{
+
+	}
+
+	void UpdateDraw(Vector3 handPos, Line currentLine)
+	{
+
+	}
+}
