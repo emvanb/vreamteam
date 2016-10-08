5745f96088155f467c0eda248d6ef841932f60bd
tinashe <tinashechagwedera@gmail.com>
2016-10-08 11:13:36 -0400

    woops

--- PitchPaint/Assets/Scripts/Point.cs
+++ PitchPaint/Assets/Scripts/Point.cs
@@ -8,34 +8,6 @@ public class LinePoint
 
 
 	// contains vector3 to for ponit location adn vector3 for velocity
-	public Vector3 PointLocation{
-		get;
-		set;
-	}
-
-	public Vector3 Velocity{ // velocity of point
-		get;
-		set;
-
-	}
-
-	public float CreationTime {
-		get;
-		set;
-
-	}
-		
-}
-using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
-using System;
-
-public class LinePoint
-{
-
-
-	// contains vector3 to for ponit location adn vector3 for velocity
 	public Vector3 pointLocation{
 		get;
 		set;
@@ -52,5 +24,6 @@ public class LinePoint
 		set;
 
 	}
-		
+
 }
+
