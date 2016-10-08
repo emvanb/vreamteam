2696b306692ee04a4d64b5b789eb5a1c1048003c
TChag <tinashechagwedera@gmail.com>
2016-10-08 11:05:37 -0400

    camelCase on Point

--- PitchPaint/Assets/Scripts/Point.cs
+++ PitchPaint/Assets/Scripts/Point.cs
@@ -26,3 +26,31 @@ public class LinePoint
 	}
 		
 }
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class LinePoint
+{
+
+
+	// contains vector3 to for ponit location adn vector3 for velocity
+	public Vector3 pointLocation{
+		get;
+		set;
+	}
+
+	public Vector3 pointVelocity{ // velocity of point
+		get;
+		set;
+
+	}
+
+	public float creationTime { // creation time 
+		get;
+		set;
+
+	}
+		
+}
