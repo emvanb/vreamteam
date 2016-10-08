003dca76c18902f711ac753508978f57d9cfe295
tinashe <tinashechagwedera@gmail.com>
2016-10-08 14:45:52 -0400

    mouse test scene and modifiation to scripts

---
+++ PitchPaint/Assets/Scripts/LinePoint.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class LinePoint: MonoBehaviour {
+
+	public AudioSource sample;
+	// contains vector3 to for ponit location adn vector3 for velocity
+	public Vector3 pointLocation{
+		get;
+		set;
+	}
+
+	public Vector3 pointVelocity{ // velocity of point
+		get;
+		set;
+
+	}
+
+	public float creationTime { // creation time 
+		get;
+		set;
+
+	}
+
+}
+
